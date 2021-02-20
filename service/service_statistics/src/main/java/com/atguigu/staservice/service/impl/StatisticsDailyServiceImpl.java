737573d9549e1ebed5823837a7a71305caab1558
longyi5243 <525545144@qq.com>
2021-02-20 23:47:27 +0800

    no message

--- service/service_statistics/src/main/java/com/atguigu/staservice/service/impl/StatisticsDailyServiceImpl.java
+++ service/service_statistics/src/main/java/com/atguigu/staservice/service/impl/StatisticsDailyServiceImpl.java
@@ -5,6 +5,7 @@ import com.atguigu.staservice.client.UcenterClient;
 import com.atguigu.staservice.entity.StatisticsDaily;
 import com.atguigu.staservice.mapper.StatisticsDailyMapper;
 import com.atguigu.staservice.service.StatisticsDailyService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,12 @@ public class StatisticsDailyServiceImpl extends ServiceImpl<StatisticsDailyMappe
 
     @Override
     public void registerCount(String day) {
+
+        //添加记录之前，把统计表中相同日期的记录删除，保证统计表中每个日期只有一个
+        QueryWrapper<StatisticsDaily> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("date_calculated", day);
+        baseMapper.delete(queryWrapper);
+
         //远程调用Ucenter模块，查询某个日期注册的人数
         R registerR = ucenterClient.countRegister(day);
         Map<String, Object> data = registerR.getData();
